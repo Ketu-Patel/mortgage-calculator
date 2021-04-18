6d48781bfff5a2e8aad34ce41145e1a2b9ebb2ba
Ketu-Patel <ketupatel1010@gmail.com>
2021-04-18 02:32:32 -0400

    Added more functionality
    Created input fields that take in the data and work with the javascript file.

--- source.js
+++ source.js
@@ -1,4 +1,15 @@
-function myFunction(){
-    document.getElementById("calculate").innerHTML ="Done";
+
+
+function myFunction(addForm){
+   
+    document.getElementById("calculate").innerHTML ="Done";    
     
+    var loan = addForm.loan.value;
+    var term = addForm.term.value;
+    var rate = addForm.interest.value/100;
+    rate /=12;
+    var payment = loan*((rate*(1+rate)**term))/(((1+rate)**term) -1);
+    payment=Math.round(payment*100);
+    payment = payment/100;
+    document.getElementById("hehe").innerHTML="$"+payment;
 }
